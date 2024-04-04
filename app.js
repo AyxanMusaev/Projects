55f5f562eb77bc067b9aabe4d2a4f42b7def7a88
AyxanMusaev <122926437+AyxanMusaev@users.noreply.github.com>
2024-04-04 12:53:46 +0400

    slide window

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slideSection = document.querySelector('.slide-section');
-    const boxWidth = slideSection.querySelector('.box').offsetWidth; // Bir kutunun genişliği
-    const leftBtn = document.querySelector('.left.btn');
-    const rightBtn = document.querySelector('.right.btn');
-    
-    // Sol butona tıklandığında
-    leftBtn.addEventListener('click', function() {
-        // slideSection'ın scroll pozisyonunu bir kutu kadar sola kaydır
-        slideSection.scrollLeft -= boxWidth;
-        
-        // Eğer scroll pozisyonu negatif ise (ilk kutuda iseniz), son kutuya geç
-        if (slideSection.scrollLeft <= 0) {
-            slideSection.scrollLeft = slideSection.scrollWidth - slideSection.clientWidth;
-        }
-    });
-    
-    // Sağ butona tıklandığında
-    rightBtn.addEventListener('click', function() {
-        // slideSection'ın scroll pozisyonunu bir kutu kadar sağa kaydır
-        slideSection.scrollLeft += boxWidth;
-        
-        // Eğer scroll pozisyonu sona ulaştıysa, başa dön
-        if (slideSection.scrollLeft >= slideSection.scrollWidth - slideSection.clientWidth) {
-            slideSection.scrollLeft = 0;
-        }
-    });
-});
+let panels=document.querySelectorAll('.panel')
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel)=>{
+        panel.classList.remove('active')
+    })
+}
+
